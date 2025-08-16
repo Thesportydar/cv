25eabec95de3fb7130894eacda3944d71223b0b1
Iñaqui Paladino <ipaladinobravo@gmail.com>
2025-08-16 17:00:10 -0300

    personalInfo en ingles

--- personalInfo.ts
+++ personalInfo.ts
@@ -17,6 +17,6 @@ export const personalInfoT = {
     portfolio: "https://inaqui.me",
     ciudad: "Tandil, Buenos Aires, Argentina",
     edad: 23,
-    profile: "Ingeniero de Sistemas, actualmente trabajando en procesamiento de datos, automatización y desarrollo backend orientado a la toma de decisiones. Cuento con trayectoria en equipos ágiles (Scrum, SAFe), destacándome por mi capacidad de adaptación, aprendizaje continuo y resolución de problemas. Apasionado por optimizar procesos, automatizar flujos de trabajo y explorar nuevas herramientas a través de proyectos propios y colaborativos."
+    profile: "Systems Engineer, currently working in data processing, automation, and backend development focused on decision-making. I have experience in agile teams (Scrum, SAFe), and I'm recognized for my adaptability, continuous learning, and problem-solving skills. I'm passionate about optimizing processes, automating workflows, and exploring new tools through personal and collaborative projects."
   }
 };
