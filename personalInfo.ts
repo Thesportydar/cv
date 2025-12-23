25eaf538f2e45b18da31f3bd8d40c4870abc0819
Iñaqui Paladino <ipaladinobravo@gmail.com>
2025-12-23 16:53:37 -0300

    new domain name

--- personalInfo.ts
+++ personalInfo.ts
@@ -4,7 +4,7 @@ export const personalInfoT = {
     role: "Ingeniero de Sistemas",
     email: "ipaladinobravo@gmail.com",
     linkedin: "https://www.linkedin.com/in/ipaladino",
-    portfolio: "https://inaqui.me",
+    portfolio: "https://inaquipaladino.com",
     ciudad: "Tandil, Buenos Aires, Argentina",
     edad: 23,
     profile: "Ingeniero de Sistemas, actualmente trabajando en procesamiento de datos, automatización y desarrollo backend orientado a la toma de decisiones. Cuento con trayectoria en equipos ágiles (Scrum, SAFe), destacándome por mi capacidad de adaptación, aprendizaje continuo y resolución de problemas. Apasionado por optimizar procesos, automatizar flujos de trabajo y explorar nuevas herramientas a través de proyectos propios y colaborativos."
@@ -14,7 +14,7 @@ export const personalInfoT = {
     role: "Systems Engineer",
     email: "ipaladinobravo@gmail.com",
     linkedin: "https://www.linkedin.com/in/ipaladino",
-    portfolio: "https://inaqui.me",
+    portfolio: "https://inaquipaladino.com",
     ciudad: "Tandil, Buenos Aires, Argentina",
     edad: 23,
     profile: "Systems Engineer, currently working in data processing, automation, and backend development focused on decision-making. I have experience in agile teams (Scrum, SAFe), and I'm recognized for my adaptability, continuous learning, and problem-solving skills. I'm passionate about optimizing processes, automating workflows, and exploring new tools through personal and collaborative projects."
